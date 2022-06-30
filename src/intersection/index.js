9c412890cef2ce5de428f103a95a89703db3fb20
Meg Riley <megriley3@gmail.com>
2022-06-30 16:18:54 -0500

    wrote solutions

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,14 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+    const aValues = new Map();
+    const sharedValues = new Set();
+
+    a.forEach((element) => aValues.set(element));
+
+    b.forEach((element) => {
+        if(aValues.has(element)) sharedValues.add(element);
+    })
+
+    return [...sharedValues]
+}
 
 module.exports = intersection;
--- src/sameLetters/index.js
+++ src/sameLetters/index.js
@@ -2,6 +2,21 @@
  * return a Map of the unique letters and arrays of words that are made up of those letters
  * @param {array} words An array  of words
  */
-function sameLetters(words) {}
+function sameLetters(words) {
+    const results = new Map();
+    words.forEach((word) => {
+        word = word.toLowerCase();
+        const noDupes = new Set(word);
+        let letters = [...noDupes];
+        letters.sort();
+        const letString = letters.join("");
+        if(results.has(letString)){
+            results.get(letString).push(word)
+        } else {
+            results.set(letString, [word])
+        }
+    })
+    return results
+}
 
 module.exports = sameLetters;
